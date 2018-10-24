88a94c34762a100b9a719b3c2ec057d649470002
Jassi797 <jasvinder797@gmail.com>
2018-10-24 12:16:08 +0530

    Uploaded files

---
+++ FileSplit.bat
@@ -0,0 +1,2 @@
+﻿@ ehco off 
+"C:\Program Files\7-Zip\7z.exe" a -v3m "C:\Users\Perfect\Desktop\chrone job\File Splitter\splits\part.zip" "C:\Users\Perfect\Desktop\chrone job\File Splitter\sample.bak"
